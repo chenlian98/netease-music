e88366d8e9c3acdabf6496c44e58fedca64e75fc
cl <761171478@qq.com>
2021-07-18 16:02:53 +0800

    2

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  //"es2015", { modules: false }] 热更新的代码
-  presets: [["es2015", { modules: false }], ["@vue/cli-plugin-babel/preset"]],
+  presets: [ "@vue/cli-plugin-babel/preset"],
   plugins: [
     [
       "component",
